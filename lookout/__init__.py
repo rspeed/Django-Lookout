463f7995e927dcc1e028c8c699a24769546d7f02
Rob Speed <speed.rob@gmail.com>
2018-03-09 02:19:04 -0500

    DOCS: Rewrite the version-fetching code as a class and separate methods so it can be better documented.

--- lookout/__init__.py
+++ lookout/__init__.py
@@ -1,30 +1,50 @@
-import typing
+from typing import Optional
 from pkg_resources import get_distribution, DistributionNotFound
 from os import path
 
 
 
-def package_version() -> typing.Optional[str]:
+class PackageVersion:
 	""" Attempts to determine the package's version. """
 
-	# VERSION.txt
-	try:
-		with open(path.join(path.dirname(__file__), 'VERSION.txt')) as version_file:
-			return version_file.read().strip()
-	except (FileNotFoundError, PermissionError):
-		# Probably a development environment
-		pass
-
-	# pkg_resources
-	try:
-		return get_distribution('Django-Lookout').version
-	except DistributionNotFound:
-		# The package isn't installed
-		pass
-
-	return None
+	__version = None
 
 
-__version__ = package_version()
+	def __new__(cls, *args, **kwargs):
+		return cls.get()
+
+
+	@classmethod
+	def get (cls) -> str:
+		""" Attempts each method to get the package version in turn until one succeeds. """
+
+		return cls.from_file() or cls.from_distribution() or 'unknown'
+
+
+	@staticmethod
+	def from_file () -> Optional[str]:
+		""" Gets the package version from the VERSION.txt file created by ``setuptools-scm``. """
+
+		try:
+			with open(path.join(path.dirname(__file__), 'VERSION.txt')) as version_file:
+				return version_file.read().strip()
+		except (FileNotFoundError, PermissionError):
+			# Probably a development environment
+			return None
+
+
+	@staticmethod
+	def from_distribution () -> Optional[str]:
+		""" Gets the package version from its installed distribution using ``pkg_resources``. """
+
+		try:
+			return get_distribution('Django-Lookout').version
+		except DistributionNotFound:
+			# The package isn't installed
+			return None
+
+
+
+__version__ = PackageVersion()
 
 default_app_config = '{}.apps.DjangoLookoutConfig'.format(__name__)
