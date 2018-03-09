f7bf8eadb985fed34ed8805226b6c7d7fef14c6e
Rob Speed <speed.rob@gmail.com>
2018-03-09 01:39:38 -0500

    FIX: Don't import any modules unless we're actually running the tests.

--- runtests.py
+++ runtests.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
-import os
-import sys
 
-import django
-from django.conf import settings
-from django.test.utils import get_runner
-
+""" Script to run tests of a modular Django app without a project. """
 
 
 if __name__ == '__main__':
 	os.environ['DJANGO_SETTINGS_MODULE'] = 'lookout.tests.test_settings'
+	import os
+	import sys
+
+	import django
+	from django.conf import settings
+	from django.test.utils import get_runner
+
+
 	django.setup()
 
 	# Get the test runner class based on the settings
