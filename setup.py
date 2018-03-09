42d50b9033327ad03fc4ccbe0b473930d5dfad11
Rob Speed <speed.rob@gmail.com>
2018-03-09 01:41:26 -0500

    FEAT: Add 'dev' and 'docs' installation extras.

--- setup.py
+++ setup.py
@@ -16,13 +16,20 @@ setup(
 		'Django>=1.10',
 		'Pygments<3>=2.2',
 		'jsonschema<3>=2.6.0',
-		'pytz>=2017.2'
+		'pytz>=2017.2'  # Not provided with Django<=1.10
 	],
 	setup_requires=[
-		'setuptools_scm',
+		'setuptools-scm',
 		'wheel'
 	],
 	extras_require={
+		'dev': [
+			'codecov',
+			'setuptools-scm'
+		],
+		'docs': [
+			'Sphinx>=1.6'
+		]
 	},
 	python_requires='>=3.5',
 )
